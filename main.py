325046b6baeeb71e8feabaa2f43c6fde31c9e72b
NikolaZhal <ryazan62nik@yandex.ru>
2023-12-27 21:43:00 +0300

    final

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtCore import QRectF
-from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtGui import QColor, QPainter, QPen
 from PyQt5.QtWidgets import (
     QApplication,
     QPushButton,
@@ -11,7 +11,7 @@ from PyQt5.QtWidgets import (
     QMainWindow,
 )
 from PyQt5 import uic  # Импортируем uic
-
+from random import randint
 import sys
 
 
@@ -21,40 +21,7 @@ class Square1(QWidget):
         uic.loadUi("UI.ui", self)  # Загружаем дизайн
         self.draw.clicked.connect(self.paint)
         self.do_paint = False
-        self.color = QColor(255, 0, 0)
-
-    def initUI(self):
-        self.setGeometry(300, 300, 800, 900)
-        self.setWindowTitle("Квадрат объектив 1")
-
-        self.label = QLabel(self)
-        self.label.setText("side")
-        self.label.move(300, 20)
-
-        self.label1 = QLabel(self)
-        self.label1.setText("coeff")
-        self.label1.move(300, 50)
-
-        self.label2 = QLabel(self)
-        self.label2.setText("n")
-        self.label2.move(300, 80)
-
-        self.btn = QPushButton("Показать", self)
-        self.btn.resize(self.btn.sizeHint())
-        self.btn.move(30, 60)
-        self.btn.clicked.connect(self.paint)
-
-        self.lineEdit = QLineEdit(self)
-        self.lineEdit.setText("300")
-        self.lineEdit.move(370, 20)
-
-        self.lineEdit_2 = QLineEdit(self)
-        self.lineEdit_2.setText("0.9")
-        self.lineEdit_2.move(370, 50)
-
-        self.lineEdit_3 = QLineEdit(self)
-        self.lineEdit_3.setText("10")
-        self.lineEdit_3.move(370, 80)
+        self.color = QColor(255, 255, 0)
 
     def paintEvent(self, event):
         if self.do_paint:
@@ -68,17 +35,12 @@ class Square1(QWidget):
         self.repaint()
 
     def draw_flag(self, qp):
-        side, coeff, n = 100, 50, 5
-        qp.setPen(self.color)
+        n = randint(2, 8)
+        qp.setPen(QPen(self.color, 8))
         for i in range(n):
-            qp.drawRect(
-                QRectF(
-                    50 + (side - side * coeff**i) / 2,
-                    130 + (side - side * coeff**i) / 2,
-                    side * coeff**i,
-                    side * coeff**i,
-                )
-            )
+            diametr = randint(8, 300)
+            x, y = randint(75, 500 - diametr), randint(75, 500 - diametr)
+            qp.drawEllipse(x, y, diametr, diametr)
 
 
 def except_hook(cls, exception, traceback):
